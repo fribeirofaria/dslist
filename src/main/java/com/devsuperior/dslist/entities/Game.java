a1bcd0c1ed2619a34800195b953f2a606de500cf
Fernando de Faria <fribeirofaria@gmail.com>
2025-01-22 10:29:13 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -16,7 +16,7 @@ public class Game {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String titles;
+	private String title;
 	
 	@Column(name = "game_year")
 	private Integer year;
@@ -24,17 +24,21 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(Long id, String titles, Integer year, String genre, String platforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
-		this.titles = titles;
+		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
@@ -52,12 +56,12 @@ public class Game {
 		this.id = id;
 	}
 
-	public String getTitles() {
-		return titles;
+	public String gettitle() {
+		return title;
 	}
 
-	public void setTitles(String titles) {
-		this.titles = titles;
+	public void settitle(String title) {
+		this.title = title;
 	}
 
 	public Integer getYear() {
